4234d000063bcc0dd2241d3569fbb8c83b3196d7
Azhor Ahai <debapriya.das@incture.com>
2016-10-15 14:43:32 +0530

    added sha256 hashing

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ var crypto = require('crypto');
 module.exports = {
 
 	// now use var imported.md5(string) to get your md5 hashed string
+
 	// more functionality will be added soon
 	
-	'md5': function (input) { 
-		return crypto.createHash('md5').update(input).digest('hex');
+	'md5': function (input) {
+
+		return crypto.createHash('md5')
+		.update(input)
+		.digest('hex');
+	},
+	
+
+	'sha256': function(input, secret) {
+		// sha256 hashing function added
+		// by default secret hashing key is 'password'
+		// though you can pass your own
+
+		if (typeof(input)==='undefined') return ("give the input string");
+		if (typeof(secret)==='undefined') secret = "password";
+		
+		const hash = crypto.createHmac('sha256', secret)
+                   .update(input)
+                   .digest('hex');
+		return hash;
+
 	}
+
 
 };
